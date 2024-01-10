3c28810eef8cc348addeb8bde3c9520df282ac5f
rameez <rameezkhalil23@gmail.com>
2024-01-10 04:18:44 -0800

    database modeling

---
+++ Bloggie.Web/Models/Domain/Tag.cs
@@ -0,0 +1,17 @@
+﻿namespace Bloggie.Web.Models.Domain
+{
+    public class Tag
+    {
+
+        /*
+         * Name and DisplayName are two different properties.
+         * They might look similar but they have a different purpose
+         */
+
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string DisplayName { get; set; }
+
+        public ICollection<BlogPost> BlogPosts { get; set; }
+    }
+}
